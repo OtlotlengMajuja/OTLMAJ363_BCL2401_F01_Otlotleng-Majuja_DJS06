523356c8239fa035400eba78115d6b6bf8490df3
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-05-24 09:59:09 +0200

    Iterated over the products array using the 'foreach' method

---
+++ advanced.js
@@ -0,0 +1,25 @@
+// A list of provinces:
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+
+// A list of names:
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+
+// A list of products with prices:
+const products = [
+    { product: 'banana', price: "2" },
+    { product: 'mango', price: 6 },
+    { product: 'potato', price: ' ' },
+    { product: 'avocado', price: "8" },
+    { product: 'coffee', price: 10 },
+    { product: 'tea', price: '' },
+]
+
+console.log(
+    // Advanced Exercise 1
+
+    /**
+     * Log Products: Iterate over the products array, logging each product name.
+     */
+    products.forEach(product => console.log(product.product)),
+
+);
